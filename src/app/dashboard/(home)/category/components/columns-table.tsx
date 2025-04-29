d8a49f627fe2c65d618c27010291edb26e2483f3
LAPTOP-BPSKD5T7\Acer <eastyandaputra@gmail.com>
2025-04-29 12:03:15 +0700

    Create Modul Edit and Delete Category

--- src/app/dashboard/(home)/category/components/columns-table.tsx
+++ src/app/dashboard/(home)/category/components/columns-table.tsx
@@ -3,8 +3,9 @@
 import { Button } from "@/components/ui/button"
 import { Category } from "@/model/Category"
 import { ColumnDef } from "@tanstack/react-table"
-import { Pencil } from "lucide-react"
+import { Delete, Pencil } from "lucide-react"
 import Link from "next/link"
+import DeleteCategory from "./delete-category"
 
 export type Payment = {
   id: string
@@ -28,13 +29,14 @@ export const columns: ColumnDef<Category>[] = [
         const category = row.original;
 
         return (
-            <div className="inline-flex gap-5 items-center">
+            <div className="inline-flex gap-5 items-end">
                 <Button variant="secondary" size="sm" asChild>
                     <Link href={`/dashboard/category/edit/${category.id}`}>
                         <Pencil className="mr-2 h-4 w-4"/>
                         Edit
                     </Link>
                 </Button>
+                <DeleteCategory id={category.id}/>
             </div>
         )
     },
